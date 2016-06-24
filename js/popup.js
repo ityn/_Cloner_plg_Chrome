7eced7cb29dca6940716ecbee2f79ab1df4aeadb
ityn <ityn8161@gmail.com>
2016-06-24 15:08:20 +0700

    works

--- js/popup.js
+++ js/popup.js
@@ -18,8 +18,9 @@ $(document).ready(function(){
         var dt1_str = ""+dt1.getDate()+"-"+Math.round(dt1.getMonth()+1)+"-"+dt1.getFullYear();
         var dt2 = new Date($('#dt2').val());
         var dt2_str = ""+dt2.getDate()+"-"+Math.round(dt2.getMonth()+1)+"-"+dt2.getFullYear();
-
-        var addr = "https://www.ageofclones.com/journal/kalita/from_date/"+dt1_str+"/to_date/"+dt2_str+"/page/1";
+        var res_json = [];
+        var dmn = "https://www.ageofclones.com";
+        var addr = dmn+"/journal/kalita/from_date/"+dt1_str+"/to_date/"+dt2_str+"/page/1";
         $('#wrapper').html('<br><i>Полученные данные:</i></br>'+addr);
 
          xhr = new XMLHttpRequest();
@@ -33,20 +34,28 @@ $(document).ready(function(){
                  if (xhr.responseText)
                  {
                      var data = xhr.responseText;
+                     var pg_last = $('div.pager a.arr.last', data);
+                     $('#wrapper').html('<b><i>Полученные данные:</i></b>');
+                     $('#footer').html(pg_last.attr('href'));
                      var table_data = $('table#journal_entries tbody', data);
                      //var table = table_data.children('tbody');
                      var myRows = table_data.find('tr');
-                     $('#wrapper').html('<b><i>Полученные данные:</i></b>');
+
                      for (var i = 1; i < myRows.length; i++)
                      {
                          $('#wrapper').append('<br>');
+                         var res = {};
                          for (var j = 0; j < 9; j++)
                          {
                              var dt = ($(myRows[i]).find('td:eq('+j+')').html());
                              $('#wrapper').append(dt);
                              $('#wrapper').append(' | ');
                          }
+                         res.dt = $(myRows[i]).find('td:eq('+1+')').html();
+                         res.name = $(myRows[i]).find('td:eq('+2+')').html();
+                         res_json.push(res);
                      }
+                     console.log(res_json);
                  }
              }
          }
